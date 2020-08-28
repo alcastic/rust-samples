6d9bc4a35c94483f11994c396c85f98c1dc5bb2f
Alexander Castillo <alcastic@gmail.com>
2020-08-27 22:19:19 -0400

    Add comparation to guessing number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,17 +1,24 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("=== Guess the number ===");
 
-    let _secret_number = rand::thread_rng().gen_range(1, 101);
+    let _secret_number = rand::thread_rng().gen_range(1, 11);
 
-    println!("Please input your guess:");
+    println!("Please input your guess (from 1 to 10):");
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("Your guess: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Input is not a number");
+
+    match guess.cmp(&_secret_number) {
+        Ordering::Less => println!("Your guess: {} is less tha secret_number", guess),
+        Ordering::Equal => println!("Your guess: {} is less tha secret_number", guess),
+        Ordering::Greater => println!("Your guess: {} is less tha secret_number", guess),
+    }
 }
