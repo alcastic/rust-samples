cad2240ade4a43336574d8674fc1fa1b130f1195
Alexander Castillo <alcastic@gmail.com>
2020-09-01 21:38:08 -0400

    Shadowing variables.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,4 +8,15 @@ fn main() {
     let mut mutable_var: u32 = 100_000_000;
     mutable_var = mutable_var + 1;
     println!("mutable_var={}", mutable_var);
+
+    let x:i8 = 10; // declares x variable
+    println!("initial x={}", x);
+    let x:i8 = 11; // shadowing variable x
+    println!("previous x value shadowed by x={}", x);
+    let x:i32 = 12; // shadowing variable x and changing its type
+    println!("previous x value and type shadowed by x={}", x);
+
+    let mut x:u32 = 100;
+    x = x + 1;
+    println!("previous x value and type shadowed by mutable x={}", x);
 }
